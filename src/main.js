9bf04c5387d499861ac6adebf926034f5c603a24
heon <zsewa0@gmail.com>
2022-04-07 01:40:29 +0900

    file remove function changed

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ var delete_rdp_history= subkeys => {
     // if default.rdp file is existed in Documents Directory, delete default.rdp file
     var default_rdp_file_path = `${os.homedir()}\\Documents\\Default.rdp`;
     if (fs.existsSync(default_rdp_file_path)) {
-        execSync(`DEL /F /Q /A "${default_rdp_file_path}"`);
+        fs.unlinkSync(default_rdp_file_path);
     }
     
     
